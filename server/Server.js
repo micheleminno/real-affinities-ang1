251674e599ae55721e8e9ebb4f3afbe8ee1e4ac8
Michele Minno <minno@cybion.it>
2017-07-13 17:10:58 +0200

    Server files in folder 'server'

---
+++ server/Server.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var connection = require('express-myconnection');
+var mysql = require('mysql');
+
+var config = require('./config.js');
+var target = require('./target.js');
+var affinities = require('./affinities.js');
+var twitter = require('./twitter.js');
+var utilities = require('./utilities.js');
+
+
+app = express();
+
+var allowCrossDomain = function(req, res, next) {
+
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods',
+			'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers',
+			'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+
+	next();
+};
+
+app.use(allowCrossDomain);
+
+var connectionConfig = {
+
+	host : config.mysql.host,
+	user : config.mysql.user,
+	password : config.mysql.password,
+	database : config.mysql.database
+};
+
+var connection = connection(mysql, connectionConfig, 'request');
+
+app.use(connection);
+
+app.get('/', function(req, res) {
+	res.send('Welcome');
+});
+
+app.get('/target', target.list);
+app.get('/target/add', target.add);
+app.get('/target/remove', target.remove);
+app.get('/target/delete', target.removeAll);
+app.get('/target/contains', target.contains);
+
+app.get('/affinities/interesting', affinities.interesting);
+
+app.get('/twitter/search/users', twitter.searchUsers);
+app.get('/twitter/search/tweets', twitter.searchTweets);
+app.get('/twitter/tweets', twitter.userTweets);
+app.get('/twitter/users', twitter.users);
+
+app.get('/utilities/url-exists', utilities.checkUrl);
+
+var server = app.listen(3000, function() {
+
+	console.log("Listening to port %s", server.address().port);
+
+});
---
+++ server/affinities.js
@@ -0,0 +1,331 @@
+var Twit = require('twit');
+var fs = require("fs");
+var async = require("async");
+
+var OK = 200;
+
+exports.interesting = function(req, res) {
+
+	var offset = req.query.offset;
+	var amount = req.query.amount;
+	req.getConnection(function(err, connection) {
+
+		connection.query('SELECT id from affinity '
+				+ 'ORDER BY followed_by DESC, follows DESC LIMIT ' + offset
+				+ ',' + amount, function(err, rows) {
+
+			if (err) {
+				console.log("Error Selecting : %s ", err);
+			} else {
+
+				var ids = [];
+				for ( var rowIndex in rows) {
+
+					ids.push(rows[rowIndex]["id"]);
+				}
+
+				res.status(OK).json('interesting', {
+					interestingIds : ids
+				});
+			}
+		});
+	});
+};
+
+var userAccounts = JSON.parse(fs
+		.readFileSync("./twitter-accounts.json", "utf8"));
+
+var relationTypes = [ 'followers', 'friends' ];
+var result = {};
+
+function getTwitter(userIndex) {
+
+	return new Twit({
+		consumer_key : userAccounts[userIndex]["consumer"],
+		consumer_secret : userAccounts[userIndex]["consumerSecret"],
+		access_token : userAccounts[userIndex]["token"],
+		access_token_secret : userAccounts[userIndex]["tokenSecret"],
+	});
+};
+
+function updateAffinityValues(connection, ids, relationType, add, callback) {
+
+	var followsIncrement = 0;
+	var followedByIncrement = 0;
+	var initialFollowsValue = 0;
+	var initialFollowedByValue = 0;
+
+	if (add) {
+
+		if (relationType == "followers") {
+
+			initialFollowsValue++;
+			followsIncrement++;
+
+		} else if (relationType == "friends") {
+
+			initialFollowedByValue++;
+			followedByIncrement++;
+		}
+	} else { // remove, initial values are 0
+
+		if (relationType == "followers") {
+
+			followsIncrement--;
+
+		} else if (relationType == "friends") {
+
+			followedByIncrement--;
+		}
+	}
+
+	console.log(ids.length + " ids to update with relation type "
+			+ relationType);
+
+	async.each(ids, function(id, done) {
+
+		var query = "INSERT INTO affinity VALUES (" + id + ", "
+				+ initialFollowsValue + ", " + initialFollowedByValue
+				+ ") ON DUPLICATE KEY UPDATE " + "follows = follows + "
+				+ followsIncrement + ", followed_by = followed_by + "
+				+ followedByIncrement;
+
+		connection.query(query, function(err, rows) {
+
+			if (err) {
+
+				console.log("MySQL " + err);
+			}
+
+			done(null);
+		});
+	}, function(err) {
+
+		if (err) {
+
+			console.log('A query failed to be executed');
+
+		} else {
+
+			callback();
+		}
+	});
+};
+
+function sendResult(userId, nextPage, cursor, relationType, callback) {
+
+	var lastRetrievedPage = nextPage - 1;
+	console.log("Last retrieved page: " + lastRetrievedPage);
+
+	result[relationType]["page"] = lastRetrievedPage;
+	result[relationType]["cursor"] = cursor;
+
+	// If both loaded
+	if (Object.keys(result["followers"]).length > 0
+			&& Object.keys(result["friends"]).length > 0) {
+
+		console.log("\nAll results fetched for user " + userId);
+		callback(result);
+	}
+};
+
+function updateAffinities(userId, nextPage, lastPageToFetch, cursor,
+		credentialsIndex, relationType, add, connection, callback) {
+
+	if (nextPage <= lastPageToFetch && cursor != 0) {
+
+		var twitter = getTwitter(credentialsIndex);
+		twitter
+				.get(
+						'application/rate_limit_status',
+						{
+							resources : relationType
+						},
+						function(err, data, response) {
+
+							var results = data.resources[relationType]['/'
+									+ relationType + '/ids'];
+							remainingCalls = results['remaining'];
+							var resetDate = results['reset'];
+
+							console
+									.log('\n'
+											+ remainingCalls
+											+ ' calls to /'
+											+ relationType
+											+ '/ids remaining for credentials of '
+											+ userAccounts[credentialsIndex]["screenName"]);
+
+							if (remainingCalls > 0) {
+
+								twitter
+										.get(
+												relationType + '/ids',
+												{
+													user_id : userId,
+													cursor : cursor
+												},
+												function(err, data, response) {
+
+													if (err) {
+
+														console
+																.log("Error: "
+																		+ JSON
+																				.stringify(err));
+														console
+																.log("Affinities not updated for user: "
+																		+ userId
+																		+ ", page: "
+																		+ nextPage);
+
+														return updateAffinities(
+																userId,
+																nextPage,
+																lastPageToFetch,
+																cursor,
+																credentialsIndex,
+																relationType,
+																add,
+																connection,
+																callback);
+													} else {
+
+														cursor = data.next_cursor;
+
+														updateAffinityValues(
+																connection,
+																data.ids,
+																relationType,
+																add,
+																function() {
+
+																	console
+																			.log("\nAffinities updated for user: "
+																					+ userId
+																					+ ", page: "
+																					+ nextPage);
+
+																	console
+																			.log('Page n. '
+																					+ nextPage
+																					+ ' - '
+																					+ nextPage
+																					* 5000
+																					+ ' '
+																					+ relationType
+																					+ ' ids of user '
+																					+ userId
+																					+ ' retrieved');
+
+																	return updateAffinities(
+																			userId,
+																			++nextPage,
+																			lastPageToFetch,
+																			cursor,
+																			credentialsIndex,
+																			relationType,
+																			add,
+																			connection,
+																			callback);
+																});
+
+													}
+												});
+							} else {
+
+								console
+										.log('\nRate limits reached for call /'
+												+ relationType
+												+ '/ids and credentials of '
+												+ userAccounts[credentialsIndex]["screenName"]);
+								var now = new Date();
+								var millisecs = now.getTime();
+								var lapseOfSeconds = resetDate
+										- Math.floor(millisecs / 1000);
+								console
+										.log('These credentials will be available again for '
+												+ relationType
+												+ ' in '
+												+ lapseOfSeconds + ' seconds');
+								credentialsIndex++;
+								if (credentialsIndex < userAccounts.length) {
+
+									return updateAffinities(userId, nextPage,
+											lastPageToFetch, cursor,
+											credentialsIndex, relationType,
+											add, connection, callback);
+								} else {
+
+									console
+											.log("\nAll credentials exploited for relation type "
+													+ relationType);
+									sendResult(userId, nextPage, cursor,
+											relationType, callback);
+								}
+							}
+						});
+
+	} else {
+
+		console.log("\nAll requested data fetched for relation type "
+				+ relationType + " and user " + userId);
+
+		sendResult(userId, nextPage, cursor, relationType, callback);
+	}
+};
+
+exports.add = function(connection, userId, callback) {
+
+	result = {
+		"userId" : userId,
+		"followers" : {},
+		"friends" : {}
+	};
+
+	relationTypes.forEach(function(relationType) {
+
+		updateAffinities(userId, 1, 15, -1, 0, relationType, true, connection,
+				callback);
+	});
+};
+
+exports.remove = function(connection, userId, callback) {
+
+	result = {
+		"userId" : userId,
+		"followers" : {},
+		"friends" : {}
+	};
+
+	connection.query("SELECT * from target WHERE id = " + userId, function(err,
+			rows) {
+
+		if (err) {
+
+			console.log("Problem with MySQL" + err);
+
+		} else {
+
+			if (rows.length > 0) {
+
+				var followersPagesAmount = rows[0]["last_followers_page"];
+				var friendsPagesAmount = rows[0]["last_friends_page"];
+
+				var fetchLimits = {
+					"followers" : followersPagesAmount,
+					"friends" : friendsPagesAmount
+				};
+
+				relationTypes.forEach(function(relationType) {
+
+					updateAffinities(userId, 1, fetchLimits[relationType], -1,
+							0, relationType, false, connection, callback);
+				});
+
+			} else {
+				// User doesn't exist, nothing to do
+			}
+		}
+	});
+};
