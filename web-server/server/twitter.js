9a97472ff60aa81794a5d1f4e457676b221d5875
Michele Minno <minno@cybion.it>
2017-08-28 10:56:16 +0200

    elasticsearch added

--- web-server/server/twitter.js
+++ web-server/server/twitter.js
@@ -33,8 +33,8 @@ var URLS_AMOUNT = 3;
 var RETWEETS_AMOUNT = 3;
 var UNIGRAMS_AMOUNT = 100;
 
-var userAccounts = process.env.accounts; 
-    //JSON.parse(fs.readFileSync("./twitter-accounts.json", "utf8"));
+var userAccounts = process.env.accounts;
+//console.log(userAccounts.length + " Twitter accounts loaded");
 
 function getTwitter(userIndex) {
 
