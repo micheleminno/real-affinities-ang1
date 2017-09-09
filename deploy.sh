d61c608652b6ee1ecb332b1387b3ee3fde8d482a
Michele Minno <minno@cybion.it>
2017-09-09 22:41:01 +0200

    Deploy and db-migrations dockerfile updated

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-ssh -i "~/.ssh/MyServer.pem" deploy@ec2-18-220-103-149.us-east-2.compute.amazonaws.com << EOF
-sudo -i
-curl -L https://github.com/docker/compose/releases/download/1.16.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-docker-compose -f docker-compose.yml -p ci up --build
+ssh -T -i "~/.ssh/MyServer.pem" ubuntu@ec2-18-220-103-149.us-east-2.compute.amazonaws.com << EOF
+
+sudo docker-compose -f docker-compose.yml -p ci up --build
 EOF
